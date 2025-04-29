695b95cda21b34ad40c0c153a084a8e3c4bbe686
Nixon <75479937+Nixon-ArockiaDhas@users.noreply.github.com>
2025-04-29 21:12:09 +0530

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ import communityRoutes from './routes/community.js';
 import saleCalendar from './routes/saleCalendar.js';
 
 const app = express();
-const PORT = 3001;
+const PORT = 4000;
 app.use(cors());
 
 // Middleware to parse JSON bodies
